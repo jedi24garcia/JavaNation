1d1482030f56234e2e2c9e9bbd10224098249a17
jedi24garcia <jedi24garcia@gmail.com>
2022-10-11 21:37:41 +1300

    First commit Java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+// code below will print out the variable MyName       
+        String MyName = "Java is awesome!";
+        System.out.println("I think " + MyName);
+    }
+
+}
+
+
