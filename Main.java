dd34f3a8e129ac871376891974f1bcf2b4d4af0b
jedi24garcia <jedi24garcia@gmail.com>
2022-10-12 21:56:24 +1300

    comment update

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {      
         int x = 1000, y = 2000, z =3000;
-        System.out.println(x + y + z);
+        System.out.println(x * y * z);
 
     }
 
 }
+
+// not sure yet. But will keep practicing more. 
