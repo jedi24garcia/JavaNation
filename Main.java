839ca4d844c9388c6cc49e582d9234dfd384cdb2
jedi24garcia <jedi24garcia@gmail.com>
2022-10-13 22:17:49 +1300

    updated triangle

--- Main.java
+++ Main.java
@@ -1,10 +1,24 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {      
-        int x = 1000, y = 2000, z =3000;
-        System.out.println(x * y * z);
-
+    public static void StarTriangle(int n)
+    {
+        int a, b;
+        for (a = 0; a < n; a++) {
+            for (b = 0; b <= a; b++) {
+            System.out.print("* ");
+        }
+        System.out.println();
     }
-
+}
+    public static void main(String arg[])
+    {
+        int k = 5;
+        StarTriangle(k);
+    }        
 }
 
-// not sure yet. But will keep practicing more. 
+
+//int x = 1000, y = 2000, z =3000;
+//System.out.println(x * y * z);
+ 
